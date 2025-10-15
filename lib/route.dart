f0bbd0667b20d5bcc9d5f6ec8e06aea1ac1ae518
Vincentius <leonandaprabowo@gmail.com>
2025-10-15 20:44:57 +0700

    fatch : tambah_kegiatan form

--- lib/route.dart
+++ lib/route.dart
@@ -8,6 +8,7 @@ import 'package:jawara_pintar/screens/kegiatan/kegiatan_menu.dart';
 import 'package:jawara_pintar/screens/kegiatan/section/broadcast_daftar_section.dart';
 import 'package:jawara_pintar/screens/kegiatan/section/kegiatan_daftar_section.dart';
 import 'package:jawara_pintar/screens/kegiatan/section/pesan_warga_section.dart';
+import 'package:jawara_pintar/screens/kegiatan/section/tambah/kegiatan_tambah.dart';
 import 'package:jawara_pintar/screens/keuangan/keuangan_tab/laporan_section/cetak_laporan_section.dart';
 import 'package:jawara_pintar/screens/keuangan/keuangan_tab/laporan_section/laporan_pemasukan_section.dart';
 import 'package:jawara_pintar/screens/keuangan/keuangan_tab/laporan_section/laporan_pengeluaran_section.dart';
@@ -222,6 +223,12 @@ final GoRouter mainRouter = GoRouter(
       name: 'kegiatan_daftar',
       path: '/kegiatan_daftar',
       builder: (context, state) => const KegiatanDaftarSection(),
+      routes: [
+        GoRoute(
+          name: 'kegiatan_tambah',
+          path: 'kegiatan_tambah',
+          builder: (context, state) => const KegiatanTambah(),)
+      ]
     ),
     GoRoute(
       name: 'pesan_warga',
