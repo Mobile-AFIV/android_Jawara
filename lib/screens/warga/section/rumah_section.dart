c98c7d81c150970c9402d2406f8412c92795dd29
Vincentius <leonandaprabowo@gmail.com>
2025-10-15 08:03:27 +0700

    chore : added rumah_tambah route script

--- lib/screens/warga/section/rumah_section.dart
+++ lib/screens/warga/section/rumah_section.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:jawara_pintar/utils/app_styles.dart';
 
 class RumahSection extends StatelessWidget {
   const RumahSection({super.key});
@@ -11,6 +13,14 @@ class RumahSection extends StatelessWidget {
       ),
       body: const Center(
         child: Text("Ini Section Rumah di Menu Warga"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppStyles.primaryColor.withValues(alpha: 1),
+        foregroundColor: Colors.white,
+        onPressed: () {
+          context.pushNamed('rumah_tambah');
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
