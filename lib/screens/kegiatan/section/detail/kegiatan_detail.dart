b636de38cd7ec6f630cc8cdf8d04a8eaed5d69a0
Vincentius <leonandaprabowo@gmail.com>
2025-10-21 10:17:24 +0700

    add: display awal dan scroll

--- lib/screens/kegiatan/section/detail/kegiatan_detail.dart
+++ lib/screens/kegiatan/section/detail/kegiatan_detail.dart
@@ -12,66 +12,73 @@ class _KegiatanDetailState extends State<KegiatanDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Broadcast Detail"),
+        title: const Text("Kegiatan Detail"),
       ),
-      body: Column( 
-        children: [
-          Card(
-            margin: const EdgeInsets.all(16.0),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    'Nama Kegiatan: Rapat RT 05',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Card(
+              margin: const EdgeInsets.all(16.0),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Nama Kegiatan: Rapat RT 05',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Kategori: \nKomunitas dan Sosial',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                   SizedBox(height: 8),
-                  Text(
-                    'Deskripsi Kegiatan:\nblabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                   SizedBox(height: 8),
-                  Text(
-                    'Lokasi Kegiatan:\nBalai RW 05',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                   SizedBox(height: 8),
-                  Text(
-                    'Penanggung Jawab: \nPak RT',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  
-                  Text(
-                    'Dibuat Oleh: \nPak RT',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Text(
-                    'Dokumentasi',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Card(
-                    margin: EdgeInsets.only(top: 8.0),
-                    child: Image(
-                      image: AssetImage('assets/images/placeholder.png'),
-                      fit: BoxFit.cover,
+                    SizedBox(height: 8),
+                    Text(
+                      'Kategori:\nKomunitas dan Sosial',
+                      style: TextStyle(fontSize: 16),
                     ),
-                  ),
-                  
-                ],
+                    SizedBox(height: 8),
+                    Text(
+                      'Deskripsi Kegiatan:\nblabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla blabla',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Lokasi Kegiatan:\nBalai RW 05',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Penanggung Jawab:\nPak RT',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Dibuat Oleh:\nPak RT',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Dokumentasi:',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Card(
+                      margin: EdgeInsets.only(top: 8.0),
+                      child: Image(
+                        image: AssetImage('assets/images/placeholder.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-      ],)
+          ],
+        ),
+      ),
     );
   }
 }
